0446f8997050fc8d4dd7a7e0e0b1cc1fe0cf1e52
deny <wahyudeny780@gmail.com>
2026-02-21 02:47:04 +0700

    made connection between code and mongodb

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 type URL struct {
@@ -20,12 +22,26 @@ var urlColl *mongo.Collection
 func main() {
 	r := gin.Default()
 
-	if err := godotenv.Load(); err != nil {
+	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("Failed to load .env")
 	}
 
-	uri := os.Getenv("")
+	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
 		log.Fatal("MONGODB URI is empty, check if its exist or not")
 	}
+
+	var err error
+	client, err := mongo.Connect(options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	urlColl = client.Database("url-shortner").Collection("urls")
 }
